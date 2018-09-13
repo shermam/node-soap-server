6feaa10964f544f8bc2c9a0384926011e7782aed
Shermam Miranda <shermam.miranda@hotmail.com>
2018-09-13 16:32:01 -0300

    Tried make test work with VS ServiceReference and SOAPUI Req OK Res NOK

--- test.js
+++ test.js
@@ -1,51 +1,22 @@
 
 var soap = require('./lib/soap-server');
 
-
-function MyTestObject(){
+function MyObject() {
 }
-MyTestObject.prototype.strArg = '';
-MyTestObject.prototype.intArg = 0;
+MyObject.prototype.MeuValor = 0;
 
-
-function MyObject(){
+function MyTestService() {
 }
-MyObject.prototype.concated = '';
-MyObject.prototype.incremented = 0;
-
-function MyTestService(){
-}
-MyTestService.prototype.test1 = function(myArg1, myArg2){
-	return myArg1 + myArg2;
-};
-MyTestService.prototype.test2 = function(myArg1, myArg2){
-	return myArg1 + myArg2;
-};
-MyTestService.prototype.test3 = function(strArg, intArg){
-	var ret = new MyObject();
-	ret.concated = strArg + '[' + intArg + ']';
-	ret.incremented = intArg + 1;
-	return ret;
-};
-MyTestService.prototype.test4 = function(myTestObjectInstance){
-	return myTestObjectInstance.strArg + '[' + myTestObjectInstance.intArg + ']';
+MyTestService.prototype.Test = function (MyObject) {
+	return { MeuValor: 2134 };
 };
 
 var soapServer = new soap.SoapServer();
-var soapService = soapServer.addService('testService', new MyTestService());
+var soapService = soapServer.addService('MyTestService', new MyTestService());
 
-var test2operation = soapService.getOperation('test2');
-test2operation.setOutputType('number');
-test2operation.setInputType('myArg1', {type: 'number'});
-test2operation.setInputType('myArg2', {type: 'number'});
-
-var test3operation = soapService.getOperation('test3');
-test3operation.setOutputType(MyObject, 'MyObject');
-test3operation.setInputType('intArg', {type: 'number'});
-
-var test4operation = soapService.getOperation('test4');
-test4operation.setInputType('myTestObjectInstance', MyTestObject);
-
+var testOperation = soapService.getOperation('Test');
+testOperation.setOutputType(MyObject, 'MyObject');
+testOperation.setInputType('MyObject', MyObject);
 
 soapServer.listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
